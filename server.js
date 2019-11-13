3dee929d730e24d9cd3b6c312f50e4181dd73a2d
Victor Arowo <arowovici@gmail.com>
2019-11-13 15:01:00 +0100

    Completed MVP Requirements

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const express = require('express');
+const userRouter = require('./resources/users/router');
+
 const app = express();
+
 app.use(express.json());
 
-// app.use("/api", userRouter);
+app.use('/api', userRouter);
 
 module.exports = app;
